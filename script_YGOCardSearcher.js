54d673daa7a4faa0712c27cbd6057ed51c1b29c8
MikeWalder <mike_walder@hotmail.fr>
2021-10-13 22:49:13 +0200

    Version 3 : modification du navbar pour le responsive - affichage du nombre de résultats pour chaque requête - affichage des images de cartes

--- script_YGOCardSearcher.js
+++ script_YGOCardSearcher.js
@@ -1,9 +1,32 @@
+const dataOrder = document.querySelector('#dataOrder');
+const dataResult = document.querySelector('#dataResult');
+
 // Récupération de la valeur du champ texte
 const inputValue = document.querySelector('#inputValue');
 inputValue.addEventListener('keyup', function(e) {
     e.preventDefault();
     if(e.keyCode==13){
-        var nb=inputValue.value;
-        console.log(nb);
+        let nb=inputValue.value;
+        inputValue.value = "";
+        const urlName = `https://db.ygoprodeck.com/api/v7/cardinfo.php?language=fr&fname=${nb}`;
+        fetch(urlName).then( (response) => 
+            response.json().then((data) => {
+                console.log(data);
+
+                // Effacement de toute les cartes de la précédente recherche
+                dataResult.innerText = `Résultats : ${data.data.length}`;
+                let srcCards = [...document.querySelectorAll('.bg-dark img')];
+                for(let j=0; j<srcCards.length; j++) {
+                    document.querySelector(`#a${j}`).src = '';
+                }
+
+                // Affichage des images de cartes correspondant à la recherche entrée
+                for(let i=1; i<=`${data.data.length}`; i++) {
+                    document.querySelector(`#a${i}`).src = data.data[i].card_images[0].image_url;
+                }
+            })
+        ).catch(err => {
+            console.log('Erreur : '+err);
+        });
     }
 }, true);
