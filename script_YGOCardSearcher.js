a75352201429fc797916636bc091f859dcf0338c
MikeWalder <mike_walder@hotmail.fr>
2021-10-18 23:57:16 +0200

    Version 6.2 : Amélioration de la fonction de tri du résultat en modal

--- script_YGOCardSearcher.js
+++ script_YGOCardSearcher.js
@@ -3,27 +3,32 @@ const dataResult = document.querySelector('#dataResult');
 const bgDark = document.querySelector('.bg-dark');
 
 const classCards = document.querySelector('#classCards');
-const cardName = [...document.querySelectorAll('.cardName')];
+const cardTri = [...document.querySelectorAll('.modal-body div > button')];
+const labelTri = [...document.querySelectorAll('.modal-body > label')];
 let orderTri = '';
-/* classCards.addEventListener('click', function() {
-    alert('tri des cartes en cours');
-}) */
 
-function recupOrderTri(orderTri) {
-    return orderTri;
+
+for(let cardN = 0; cardN < cardTri.length; cardN++) {
+    cardTri[cardN].addEventListener('focus', function() {
+        orderTri = this.value;
+        dataOrder.innerText = `Ordre : ${labelTri[cardN].innerText} (${this.innerText})`;
+        console.log(orderTri);
+
+        let nb=inputValue.value;
+        const urlName = `https://db.ygoprodeck.com/api/v7/cardinfo.php?${orderTri}&language=fr&fname=${nb}`;
+        console.log(urlName);
+        fetch(urlName).then( (response) => {
+            response.json().then( (data) => {
+                searchCardsData(data);
+            })
+        }).catch(error => {
+            console.log('Erreur : '+err);
+        })
+    })
 }
 
-console.log(cardName);
 
-    for(let cardN = 0; cardN < cardName.length; cardN++) {
-        cardName[cardN].addEventListener('click', function() {
-            orderTri = this.value;
-            console.log(orderTri);
-            console.log(typeof(orderTri));
-        })
-    }
-
-const triage = recupOrderTri(orderTri);
+/* const triage = recupOrderTri(orderTri); */
 
 const inputValue = document.querySelector('#inputValue');
 inputValue.addEventListener('keyup', function(e) {
@@ -31,70 +36,19 @@ inputValue.addEventListener('keyup', function(e) {
     if(e.keyCode==13){
         let nb=inputValue.value;  // Récupération de la valeur du champ texte
         console.log(nb);
-        inputValue.value = "";  // Effacement du contenu de l'input text
-        const urlName = `https://db.ygoprodeck.com/api/v7/cardinfo.php?language=fr&fname=${nb}`;
-        console.log(urlName);
+        //inputValue.value = "";  // Effacement du contenu de l'input text
+        const urlName = `https://db.ygoprodeck.com/api/v7/cardinfo.php?${orderTri}&language=fr&fname=${nb}`;
+        //console.log(urlName);
         fetch(urlName).then( (response) => {
             if(response.status >= 200 && response.status <= 299) {
                 response.json().then((data) => {
-                    console.log(data);
-                    console.log(response.status);
+
+                    // console.log(data);
+                    // console.log(response.status);
+
                     // Effacement de toute les cartes de la précédente recherche
-
-                    if(data.data.length) {
-                        dataResult.innerText = `Résultats : ${data.data.length}`;
-                        console.log(typeof(data));
-                    } else {
-                        dataResult.innerText = `Résultats : aucun`;
-                    }
-                    let srcCards = [...document.querySelectorAll('.bg-dark img')];
-                    for(let i=0; i<srcCards.length; i++) {
-                        document.querySelector(`#a${i}`).src = '';
-                    }
-
-                    let tabRoww = [];
-                    let disp = '';
-
-                    // Suppression de tous les éléments enfants de la liste des résultats lors de la précédente recherche
-                    while (bgDark.firstChild) {
-                        bgDark.removeChild(bgDark.lastChild);
-                    }
-
-                    // Insertion automatique de chaque élément de la liste répondant à la recherche
-                    for(let k=0; k<`${data.data.length}`; k++) {
-
-                        const colContainer = `<div class="col-xs-4 col-lg-2 p-0 hovertexter">`;
-                        disp = '';
-                        if(k%6==0) {
-                            
-                            const roww = document.createElement('div');
-                            roww.classList.add('row');
-                            roww.id = `ro${k}`;
-                            tabRoww[k] = roww;
-                            bgDark.appendChild(tabRoww[k]);
-                            
-                            for(let j=0; j<6; j++) {
-                                disp += colContainer;
-                                disp += `<img class="card-img-top" id="a${k + j}">`;
-                                disp += `<div class="" id="b${k + j}"></div>`;
-                                disp += `<div class="" id="c${k + j}"></div>`;
-                                disp += '</div>';
-                            }
-                            roww.innerHTML = disp;
-                        }
-
-                        if(data.data[k]) { // si l'indice du tableau JSON contient des données
-                            document.querySelector(`#a${k}`).src = data.data[k].card_images[0].image_url;
-                            document.querySelector(`#a${k}`).title = data.data[k].name;
-                            document.querySelector(`#a${k}`).alt = data.data[k].name;
-                            
-                            document.querySelector(`#b${k}`).classList = 'hoverCardTitle';
-                            document.querySelector(`#b${k}`).innerHTML = data.data[k].name;
-        
-                            document.querySelector(`#c${k}`).classList = 'hoverCardDescription';
-                            document.querySelector(`#c${k}`).innerHTML = `${data.data[k].race} / ${data.data[k].type}`;
-                        } 
-                    }
+                    searchCardsData(data);
+                    
                 })
             } else {
                 dataResult.innerText = `Résultats : aucun`;
@@ -102,10 +56,73 @@ inputValue.addEventListener('keyup', function(e) {
                     bgDark.removeChild(bgDark.lastChild);
                 }
             }
-        }).catch(err => {
+        }).then( (response) => {
+            
+        })
+        .catch(err => {
             console.log('Erreur : '+err);
         });
     }
 }, true);
 
 
+
+
+
+function searchCardsData(data) {
+    if(data.data.length) {
+        dataResult.innerText = `Résultats : ${data.data.length}`;
+        console.log(typeof(data));
+    } else {
+        dataResult.innerText = `Résultats : aucun`;
+    }
+
+    let srcCards = [...document.querySelectorAll('.bg-dark img')];
+    for(let i=0; i<srcCards.length; i++) {
+        document.querySelector(`#a${i}`).src = '';
+    }
+
+    let tabRoww = [];
+    let disp = '';
+
+    // Suppression de tous les éléments enfants de la liste des résultats lors de la précédente recherche
+    while (bgDark.firstChild) {
+        bgDark.removeChild(bgDark.lastChild);
+    }
+
+    // Insertion automatique de chaque élément de la liste répondant à la recherche
+    for(let k=0; k<`${data.data.length}`; k++) {
+
+        const colContainer = `<div class="col-xs-4 col-lg-2 p-0 hovertexter">`;
+        disp = '';
+        if(k%6==0) {
+            
+            const roww = document.createElement('div');
+            roww.classList.add('row');
+            roww.id = `ro${k}`;
+            tabRoww[k] = roww;
+            bgDark.appendChild(tabRoww[k]);
+            
+            for(let j=0; j<6; j++) {
+                disp += colContainer;
+                disp += `<img class="card-img-top" id="a${k + j}">`;
+                disp += `<div class="" id="b${k + j}"></div>`;
+                disp += `<div class="" id="c${k + j}"></div>`;
+                disp += '</div>';
+            }
+            roww.innerHTML = disp;
+        }
+
+        if(data.data[k]) { // si l'indice du tableau JSON contient des données
+            document.querySelector(`#a${k}`).src = data.data[k].card_images[0].image_url;
+            document.querySelector(`#a${k}`).title = data.data[k].name;
+            document.querySelector(`#a${k}`).alt = data.data[k].name;
+            
+            document.querySelector(`#b${k}`).classList = 'hoverCardTitle';
+            document.querySelector(`#b${k}`).innerHTML = data.data[k].name;
+
+            document.querySelector(`#c${k}`).classList = 'hoverCardDescription';
+            document.querySelector(`#c${k}`).innerHTML = `${data.data[k].race} / ${data.data[k].type}`;
+        } 
+    }
+}
